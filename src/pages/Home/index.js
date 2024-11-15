29a9de9728e17dc4a223350a032fe6fff085774f
Pauline Duarte <Paulineduarte1@hotmail.com>
2024-11-15 00:27:04 -0300

    update fetch

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,73 @@
-import {Header} from '../../components/Header/index.js';
+import { useState } from 'react';
+import { Header } from '../../components/Header/index.js';
 import background from '../../assets/bg.png';
 import './styles.css';
 import ItemList from '../../components/ItemList'
 
 function App() {
+  const [user, setUser] = useState('');
+  const [currentuser, setCurrentUser] = useState(null);
+  const [reposit, setReposit] = useState(null);
+
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`)
+    const newUser= await userData.json();
+
+    if (newUser.name){
+      const {avatar_url, name , bio, login} = newUser;
+      setCurrentUser({avatar_url, name , bio, login})
+
+      console.log(newUser);
+
+      const repositData = await fetch(`https://api.github.com/users/${user}/repos`)
+      const newReposit= await repositData.json();
+
+      if (newReposit.length){
+        setReposit(newReposit);
+      }
+
+
+    }
+
+
+  };
+
+
   return (
     <div className="App">
       <Header />
       <div className="conteudo">
-        <img src={background} className="background" alt="imagem de background"/>
-        <div className="info"> 
+        <img src={background} className="background" alt="imagem de background" />
+        <div className="info">
           <div>
-          <input name="usuario" placeholder="@username"/>
-          <button>Buscar</button>
+            <input name="usuario" value={user} 
+             onChange={event => setUser(event.target.value)}
+             placeholder="@username" />
+            <button onClick={handleGetData}>Buscar</button>
           </div>
-          <div className='perfil'>
-            <img src="https://avatars.githubusercontent.com/u/104395768?s=400&u=fe2f7e70ac28274db46ede0ad9d00b8a9c3f724a&v=4" className='profile' alt='Imagem de Perfil'/>
+          {currentuser?.name ? (<>
+            <div className='perfil'>
+            <img src={currentuser.avatar_url} className='profile' alt='Imagem de Perfil' />
             <div>
-            <h3> Pauline Duarte</h3>
-            <span>@PaulineDuart</span>
-            <p>Descrição </p>
+              <h3>{currentuser.name}</h3>
+              <span>@{currentuser.login}</span>
+              <p>{currentuser.bio}</p>
             </div>
           </div>
-          <hr/>
-          <div>
+          <hr />
+          </>): null}
+          {reposit?.length ?(
+            <div>
             <h4 className='repositorio'>Repositórios</h4>
-            <ItemList title="Teste" description="descrição1"/>
+            {reposit.map( repo => (
+              <ItemList title={repo.name} description={repo.description} />
+            ))}
           </div>
-          </div>
+          ): null}
         </div>
       </div>
+    </div>
   );
 }
 
