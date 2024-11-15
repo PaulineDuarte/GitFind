0ec189f5242a75e14f2a50ccc20d3f301f3504e3
Pauline Duarte <Paulineduarte1@hotmail.com>
2024-11-14 23:34:06 -0300

    Adicionando elementos ao Gitfind

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,35 @@
-import { Header } from "../../components/Header";
-import background from '../../assets/background.png'
+import {Header} from '../../components/Header/index.js';
+import background from '../../assets/bg.png';
 import './styles.css';
+import ItemList from '../../components/ItemList'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <div className="conteudo">
-        < img src={background} className="background" alt="imagem de background"/>
-        <div className="info"> teste </div>
-        <div>
+        <img src={background} className="background" alt="imagem de background"/>
+        <div className="info"> 
+          <div>
           <input name="usuario" placeholder="@username"/>
           <button>Buscar</button>
+          </div>
+          <div className='perfil'>
+            <img src="https://avatars.githubusercontent.com/u/104395768?s=400&u=fe2f7e70ac28274db46ede0ad9d00b8a9c3f724a&v=4" className='profile' alt='Imagem de Perfil'/>
+            <div>
+            <h3> Pauline Duarte</h3>
+            <span>@PaulineDuart</span>
+            <p>Descrição </p>
+            </div>
+          </div>
+          <hr/>
+          <div>
+            <h4 className='repositorio'>Repositórios</h4>
+            <ItemList title="Teste" description="descrição1"/>
+          </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
